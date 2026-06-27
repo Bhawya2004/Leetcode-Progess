0e47723fe0296723d0d029b84c5eda5d82b77c7f
Bhawya2004 <146432148+Bhawya2004@users.noreply.github.com>
2026-06-28 01:30:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.06%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long temp=x;
+        long long  rev=0;
+        while(temp>0){
+            int digit=temp%10;
+            rev=rev*10+digit;
+            temp=temp/10;
+        }
+        if(rev==x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
