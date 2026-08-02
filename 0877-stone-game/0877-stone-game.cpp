afbf1f49b3d040167cb33b4abbfbaa41e2fcfb31
Bhawya2004 <146432148+Bhawya2004@users.noreply.github.com>
2026-08-02 11:08:02 +0530

    Time: 11 ms (27.05%), Space: 11.6 MB (34.97%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int t[501][501]; //?
+    int solve(vector<int>& nums,int i,int j){
+        if(i>j)return 0;
+        if(t[i][j]!=-1){    //?
+            return t[i][j];
+        }
+        int take_i=nums[i]+min(solve(nums,i+1,j-1),solve(nums,i+2,j));
+        int take_j=nums[j]+min(solve(nums,i+1,j-1),solve(nums,i,j-2));
+        return t[i][j]=max(take_i,take_j);
+    }
+    bool stoneGame(vector<int>& nums) {
+        int n=nums.size();
+        memset(t,-1,sizeof(t)); //?
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        int alice=solve(nums,0,n-1);
+        int bob=sum-alice;
+        if(alice>bob)return true;
+        else return false;
+    }
+};
